200ce8274b2aafd91171b3dfbffff5e67cf5f4af
Mr. Fleming <efleming@stpatrick.org>
2017-11-30 08:04:13 -0600

    Add files via upload

---
+++ ChargingDock.js
@@ -0,0 +1,18 @@
+//defines how all charging stations work.
+function ChargingDock(){
+
+//Instance variables
+
+
+
+//Instance Fucntions
+
+
+
+}
+
+//defines the testing code
+function main(){}
+
+//runs the main code
+main();
---
+++ Device.js
@@ -0,0 +1,16 @@
+//Defines how all Devices with work.
+
+function Device(){
+
+    //Instance Variables
+
+
+    //Instance Functions
+
+
+}
+
+//defines the testing code.
+function main(){}
+//runs the testing code.
+main();
