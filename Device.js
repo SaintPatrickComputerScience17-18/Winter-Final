416e8f2a5459de487c1caef712f467f84415f8a1
Mr. Fleming <efleming@stpatrick.org>
2017-12-03 23:10:03 -0600

    added some code from Friday's overview.

--- Device.js
+++ Device.js
@@ -1,14 +1,20 @@
 //Defines how all Devices with work.
 
-function Device(){
+function Device(t,ma,c){
 
     //Instance Variables
-
+    this.type = t;
 
     //Instance Functions
+    this.on = function(){
+        if(this.state == "off" && this.juice >0){
+           this.state = "idle";
+        }
+    };
+    
+    
 
-
-}
+}//end of the device declaration
 
 //defines the testing code.
 function main(){}
