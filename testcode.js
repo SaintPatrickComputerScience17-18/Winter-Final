367236cb877e5b0fd94e3ac26034abbd8b95f452
Mr. Fleming <efleming@stpatrick.org>
2017-12-13 09:45:03 -0600

    added charging dock test

--- testcode.js
+++ testcode.js
@@ -16,3 +16,28 @@ function main(){
     flemphone.charge(1000);
     console.log(flemphone.power());
 }
+
+function testdoc(){
+    let cd = new ChargingDock();
+    
+    let d1 = new Device("phone",3000,10000);
+    let d2 = new Device("laptop",3000,15000);
+    let d3 = new Device("laptop",5000,15000);
+    let d4 = new Device("tablet",3000,15000);
+    
+    d1.use(90);
+    d2.use(120);
+    d3.use(90);
+    d4.use(240);
+    
+    cd.plug(d1);
+    cd.plug(d2);
+    cd.plug(d3);
+    cd.plug(d4);
+    
+    cd.chargeAll(60);
+    
+    cd.unplug(0);
+    cd.unplug(1);
+
+}
